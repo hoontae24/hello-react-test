59bd3c5ec878fa8efc458b6a66c43d10b434633c
hoontae24 <kadr8941@gmail.com>
2022-02-28 23:13:34 +0900

    feat: Change the hook for todos on TodoApp.tsx

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,51 +1,35 @@
+import { VFC } from "react";
+
 import TodoForm from "@/components/TodoForm";
 import TodoList from "@/components/TodoList";
-import { useCallback, useState, VFC } from "react";
+import { useTodoStore } from "@/hooks/todo";
 
 export interface TodoAppProps {}
 
 const TodoApp: VFC<TodoAppProps> = (props) => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "TDD 배우기",
-      done: true,
-    },
-    {
-      id: 2,
-      text: "react-testing-library 사용하기",
-      done: true,
-    },
-  ]);
-
-  const handleInsert = useCallback((text: string) => {
-    setTodos((prev) => {
-      const nextId =
-        prev.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1;
-      return [...prev, { id: nextId, text, done: false }];
-    });
-  }, []);
-
-  const handleToggle = useCallback((id: number) => {
-    setTodos((prev) => {
-      const target = prev.find((todo) => todo.id === id);
-      if (target) {
-        target.done = !target.done;
-      }
-      return prev.slice();
-    });
-  }, []);
-
-  const handleRemove = useCallback((id: number) => {
-    setTodos((prev) => {
-      return prev.filter((todo) => todo.id !== id);
-    });
-  }, []);
+  const todoStore = useTodoStore({
+    initialTodos: [
+      {
+        id: 1,
+        text: "TDD 배우기",
+        done: true,
+      },
+      {
+        id: 2,
+        text: "react-testing-library 사용하기",
+        done: true,
+      },
+    ],
+  });
 
   return (
     <div>
-      <TodoForm onInsert={handleInsert} />
-      <TodoList todos={todos} onToggle={handleToggle} onRemove={handleRemove} />
+      <TodoForm onInsert={todoStore.onInsert} />
+      <TodoList
+        todos={todoStore.todos}
+        onToggle={todoStore.onToggle}
+        onRemove={todoStore.onRemove}
+      />
     </div>
   );
 };
